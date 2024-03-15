7d21d678210d76861c40539538b921593b0c47d2
‪Avi Brodetsky‬‏ <95278057+avib1997@users.noreply.github.com>
2024-03-15 15:04:34 +0200

    Update ContactNewButton.js

--- src/components/ContactNewButton.js
+++ src/components/ContactNewButton.js
@@ -1,18 +1,25 @@
+// ContactNewButton.js
+
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPlus, faRandom } from "@fortawesome/free-solid-svg-icons"; // Import the desired icons
-import "../styles/Contact.css"; // Import the Contact component styles
+import { faUserPlus, faRandom } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
+import "../styles/Contact.css";
 
 function ContactNewButton() {
+  const history = useHistory();
+
+  const handleAddContact = () => {
+    history.push("/contacts/new");
+  };
+
   return (
     <div className="contact-new">
-      <button>
-        <FontAwesomeIcon icon={faUserPlus} />{" "}
-        {/* Use FontAwesomeIcon component for the user plus icon */}
+      <button onClick={handleAddContact}>
+        <FontAwesomeIcon icon={faUserPlus} />
       </button>
       <button style={{ marginLeft: "15px" }}>
-        <FontAwesomeIcon icon={faRandom} />{" "}
-        {/* Use FontAwesomeIcon component for the random icon */}
+        <FontAwesomeIcon icon={faRandom} />
       </button>
     </div>
   );
