57c6dfd283b3c39e773e63db68fd4a8870fbc764
‪Avi Brodetsky‬‏ <95278057+avib1997@users.noreply.github.com>
2024-03-15 15:02:37 +0200

    Update AddContact.js

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,9 +1,40 @@
-// AddContact.js
-
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import { addContact } from "../redux/actions";
 import "../styles/AddContact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 
-function AddContact() {
+function AddContact({ addContact }) {
+  const history = useHistory();
+  const [contactData, setContactData] = useState({
+    name: "",
+    phone: "",
+    title: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContactData({
+      ...contactData,
+      [name]: value,
+    });
+  };
+
+  const handleSave = () => {
+    // Dispatch the addContact action to update the Redux store
+    addContact(contactData);
+
+    // After saving, navigate back to the main page
+    history.push("/");
+  };
+
+  const handleCancel = () => {
+    // Navigate back to the main page
+    history.push("/");
+  };
+
   return (
     <div className="contact-container">
       <div className="new-contact-container">
@@ -12,31 +43,51 @@ function AddContact() {
             src="https://randomuser.me/api/portraits/men/81.jpg"
             alt="Avatar"
           />
-          <button>
-            <i className="fa fa-refresh" aria-hidden="true"></i>
+          <button style={{ marginLeft: "15px" }}>
+            <FontAwesomeIcon icon={faSync} />{" "}
+            {/* Use FontAwesomeIcon for the refresh icon */}
           </button>
         </div>
         <div className="new-contact-inputs">
           <div className="new-contact-input">
             <label>Name</label>
-            <input />
+            <input
+              type="text"
+              name="name"
+              value={contactData.name}
+              onChange={handleChange}
+            />
           </div>
           <div className="new-contact-input">
             <label>Phone</label>
-            <input />
+            <input
+              type="text"
+              name="phone"
+              value={contactData.phone}
+              onChange={handleChange}
+            />
           </div>
           <div className="new-contact-input">
             <label>Title</label>
-            <input />
+            <input
+              type="text"
+              name="title"
+              value={contactData.title}
+              onChange={handleChange}
+            />
           </div>
         </div>
         <div className="new-contact-buttons">
-          <button className="button-ok">Save</button>
-          <button className="button-cancel">Cancel</button>
+          <button className="button-ok" onClick={handleSave}>
+            Save
+          </button>
+          <button className="button-cancel" onClick={handleCancel}>
+            Cancel
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-export default AddContact;
+export default connect(null, { addContact })(AddContact);
