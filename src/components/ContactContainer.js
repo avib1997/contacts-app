8b4ca6b39da6812ab1b47ca9a2d424bd24804798
‪Avi Brodetsky‬‏ <95278057+avib1997@users.noreply.github.com>
2024-03-15 15:03:26 +0200

    Update ContactContainer.js

--- src/components/ContactContainer.js
+++ src/components/ContactContainer.js
@@ -1,12 +1,10 @@
 import React from "react";
-import SearchInput from "./SearchInput";
 import ContactList from "./ContactList";
 import ContactNewButton from "./ContactNewButton";
 
 function ContactContainer() {
   return (
     <div className="contact-container">
-      {/* <SearchInput /> */}
       <ContactList />
       <ContactNewButton />
     </div>
